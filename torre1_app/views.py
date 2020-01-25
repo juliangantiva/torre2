aabe94ec57fa27a5222ffe3e67ecc4691e91451f
juliangantiva <juliangantiva@outlook.com>
2020-01-25 10:42:57 -0500

    Cleaning bugs

--- torre1_app/views.py
+++ torre1_app/views.py
@@ -14,8 +14,6 @@ from .models import Opportunity
 
 
 
-
-
 def list_jobs(request):
 
     opportunities = Opportunity.objects.filter(active=True).all().order_by('id')
@@ -45,49 +43,6 @@ def get_job(request):
 
 def get_data(request):
 
-    #################################################################################
-    #username = 'juliangantiva'
-    #username = 'manolo'
-    #params_con = {'deep': '2'} # grados de conexion
-    
-    #job_id = "QweRLKd9"
-    #job_id = "0wxnG7r2"   # remoto sin horario
-    #job_id = "ldENkbr7"   # no remoto, sueldo fijo
-    #job_id = "ldEVBjr7"   # no sueldo
-    #job_id = "awKxOpdn"   # Freelancer
-    #job_id = "0wxnvpr2"   # Pasantia
-    
-    # params_peo = {'offset': '0', 'size': '1', 'aggregate':'true'}
-    # params_opp = {'offset': '0', 'size': '1', 'aggregate':'true'}
-
-    #url_bio = "https://torre.bio/api/bios/{}".format(username)
-
-    #url_con = "https://torre.bio/api/people/{}/network".format(username)
-
-    # url_job = "https://torre.co/api/opportunities/{}".format(job_id)
-
-    # url_opp = "https://search.torre.co/opportunities/_search/"
-    
-    # url_peo = "https://search.torre.co/people/_search/"
-
-
-
-    # r_user = requests.get(url_bio).json()
-    #r_conec = requests.get(url_con, params=params_con).json()
-    #r_job = requests.get(url_job).json()
-    # #r_people = requests.post(url_peo, params=params_peo).json()
-    # r_oppor = requests.post(url_opp, params=params_opp).json()
-
-    # r = json.dumps(r_conec)
-    # parsed = json.loads(r)
-    # r = json.dumps(parsed, indent=4, sort_keys=False)
-    # print(r)
-
-    # f= open("conec_data.txt","w+")
-    # f.write(r)
-    # f.close() 
-    #################################################################################
-
     if request.is_ajax():
 
         username = request.GET.get('username')
@@ -110,162 +65,187 @@ def get_data(request):
             # r = json.dumps(r_user)
             # parsed = json.loads(r)
             # print(json.dumps(parsed, indent=4, sort_keys=False))
+            # f= open("conec_data.txt","w+")
+            # f.write(r)
+            # f.close()
             
 
             ##############################
             # Skills
+            
+            try:
+                user_stren  = r_user['strengths']
+                #user_indus  = r_user['opportunities'][-1]['data']
+                job_stren   = r_job['strengths']
 
-            user_stren  = r_user['strengths']
-            user_indus  = r_user['opportunities'][-1]['data']
-            job_stren   = r_job['strengths']
+                equal_stren = []
+                for job_stren_a in job_stren:
+                    equal = False
+                    for user_stren_a in user_stren:
+                        if job_stren_a['code'] == user_stren_a['code']:
+                            equal = True
 
-            equal_stren = []
-            for job_stren_a in job_stren:
-                equal = False
-                for user_stren_a in user_stren:
-                    if job_stren_a['code'] == user_stren_a['code']:
-                        equal = True
-
-                equal_stren.append(1) if equal else equal_stren.append(0)
+                    equal_stren.append(1) if equal else equal_stren.append(0)
 
 
-            for ind, job_stren_a in enumerate(job_stren):
-                if equal_stren[ind] == 0:
-                    for user_indus_a in user_indus:
-                        if job_stren_a['name'] == user_indus_a['name']:
-                            equal_stren[ind] = 2
+                # for ind, job_stren_a in enumerate(job_stren):
+                #     if equal_stren[ind] == 0:
+                #         for user_indus_a in user_indus:
+                #             if job_stren_a['name'] == user_indus_a['name']:
+                #                 equal_stren[ind] = 2
 
-            for ind, job_stren_a in enumerate(job_stren):
-                job_stren_a['accomplish'] = equal_stren[ind]
+                for ind, job_stren_a in enumerate(job_stren):
+                    job_stren_a['accomplish'] = equal_stren[ind]
+
+
+            except:
+                job_stren = { 'error': True }
 
             ##############################
             # Location
 
-            timezones   = ''
-            location_job= ''
-            time_ok     = False
-            location_ok = False
-            remote      = False
+            try:
+                timezones   = ''
+                location_job= ''
+                time_ok     = False
+                location_ok = False
+                remote      = False
 
-            job_place = r_job['place']
-            if job_place['remote'] == True:
-                remote = True
-                timezoneOffSet = int(r_user['person']['location']['timezoneOffSet'])/(1000*60*60)
-                timezones = r_job['timezones']
-                timezones_min = int(timezones[0][3:6])
-                timezones_max = int(timezones[1][3:6])
-                if timezoneOffSet >= timezones_min and timezoneOffSet <= timezones_max:
-                    time_ok = True
-            else:
-                location_user = r_user['person']['location']['name']
-                location_job = r_job['place']['location'][0]['id']
-                if location_user == location_job:
-                    location_ok = True
+                job_place = r_job['place']
+                if job_place['remote'] == True:
+                    remote = True
+                    timezoneOffSet = int(r_user['person']['location']['timezoneOffSet'])/(1000*60*60)
+                    timezones = r_job['timezones']
+                    timezones_min = int(timezones[0][3:6])
+                    timezones_max = int(timezones[1][3:6])
+                    if timezoneOffSet >= timezones_min and timezoneOffSet <= timezones_max:
+                        time_ok = True
+                else:
+                    location_user = r_user['person']['location']['name']
+                    location_job = r_job['place']['location'][0]['id']
+                    if location_user == location_job:
+                        location_ok = True
 
-            
-            job_location = {
-                'remote': remote,
-                'timezones': timezones,
-                'location_job': location_job,
-                'time_ok':time_ok,
-                'location_ok':location_ok,
-            } 
+                
+                job_location = {
+                    'remote': remote,
+                    'timezones': timezones,
+                    'location_job': location_job,
+                    'time_ok':time_ok,
+                    'location_ok':location_ok,
+                }
+
+            except:
+                job_location = { 'error': True }
+
 
             ##############################
             # Salary
 
-            salary_ok = 'blank'
-
-            compensation    =  r_job['compensation']
-            comp_code       = compensation['code']
-            comp_currency   = compensation['currency'][0:3]
-            comp_minamount  = compensation['minAmount']
-            comp_maxamount  = compensation['maxAmount']
-            comp_periodicity= compensation['periodicity']
-            opportunity     =  r_job['opportunity']
-
-            comp_minamount = to_dollar_yearly(comp_minamount, comp_currency, comp_periodicity)
-            comp_maxamount = to_dollar_yearly(comp_maxamount, comp_currency, comp_periodicity)
-
-            opportunities = r_user['opportunities']
-            if opportunity == "employee":
-                oppo_type = 'jobs'
-
-            if opportunity == "freelancer":
-                oppo_type = 'gigs'
-
-            if opportunity == "intern":
-                oppo_type = 'internships'
-
-            oppo_currency, oppo_amount, oppo_period = asign_values(opportunities,oppo_type)
-
-            oppo_amount = to_dollar_yearly(oppo_amount, oppo_currency, oppo_period)
-
-            if comp_code == 'range':
-                if comp_minamount >= oppo_amount or comp_maxamount >= oppo_amount:
-                    salary_ok = 'yes'
-                else:
-                    salary_ok = 'not'
-
-
-            if comp_code == 'fixed':
-                if comp_minamount >= oppo_amount:
-                    salary_ok = 'yes'
-                else:
-                    salary_ok = 'not'
-
-            if comp_code == 'to-be-defined':
+            try:
                 salary_ok = 'blank'
 
-            job_salary = r_job['compensation']
-            job_salary['opportunity'] = r_job['opportunity']
-            job_salary['salary_ok'] = salary_ok
+                compensation    =  r_job['compensation']
+                comp_code       = compensation['code']
+                comp_currency   = compensation['currency'][0:3]
+                comp_minamount  = compensation['minAmount']
+                comp_maxamount  = compensation['maxAmount']
+                comp_periodicity= compensation['periodicity']
+                opportunity     =  r_job['opportunity']
+
+                comp_minamount = to_dollar_yearly(comp_minamount, comp_currency, comp_periodicity)
+                comp_maxamount = to_dollar_yearly(comp_maxamount, comp_currency, comp_periodicity)
+
+                opportunities = r_user['opportunities']
+                if opportunity == "employee":
+                    oppo_type = 'jobs'
+
+                if opportunity == "freelancer" or opportunity == "consultant":
+                    oppo_type = 'gigs'
+
+                if opportunity == "intern":
+                    oppo_type = 'internships'
+
+                oppo_currency, oppo_amount, oppo_period = asign_values(opportunities,oppo_type)
+
+                oppo_amount = to_dollar_yearly(oppo_amount, oppo_currency, oppo_period)
+
+                if comp_code == 'range':
+                    if comp_minamount >= oppo_amount or comp_maxamount >= oppo_amount:
+                        salary_ok = 'yes'
+                    else:
+                        salary_ok = 'not'
+
+
+                if comp_code == 'fixed':
+                    if comp_minamount >= oppo_amount:
+                        salary_ok = 'yes'
+                    else:
+                        salary_ok = 'not'
+
+                if comp_code == 'to-be-defined':
+                    salary_ok = 'blank'
+
+                job_salary = r_job['compensation']
+                job_salary['opportunity'] = r_job['opportunity']
+                job_salary['salary_ok'] = salary_ok
+
+            except:
+                job_salary = { 'error': True }
 
             ##############################
             # Language
 
-            languages_user   = r_user['languages']
-            languages_job    =  r_job['languages']
+            try:
+                languages_user   = r_user['languages']
+                languages_job    =  r_job['languages']
 
 
-            equal_languaje = []
-            for lang_job_a in languages_job:
-                equal = False
-                for lang_user_a in languages_user:
-                    if lang_job_a['language']['code'] == lang_user_a['code']:
-                        
-                        lang1 = languaje_fluency(lang_job_a['fluency'])
-                        lang2 = languaje_fluency(lang_user_a['fluency'])
+                equal_languaje = []
+                for lang_job_a in languages_job:
+                    equal = False
+                    for lang_user_a in languages_user:
+                        if lang_job_a['language']['code'] == lang_user_a['code']:
+                            
+                            lang1 = languaje_fluency(lang_job_a['fluency'])
+                            lang2 = languaje_fluency(lang_user_a['fluency'])
 
-                        if lang1 <= lang2:
-                            equal = True
+                            if lang1 <= lang2:
+                                equal = True
 
-                equal_languaje.append(1) if equal else equal_languaje.append(0)
+                    equal_languaje.append(1) if equal else equal_languaje.append(0)
 
-            for ind, lang_job_a in enumerate(languages_job):
-                lang_job_a['accomplish'] = equal_languaje[ind]
+                for ind, lang_job_a in enumerate(languages_job):
+                    lang_job_a['accomplish'] = equal_languaje[ind]
+
+            except:
+                languages_job = { 'error': True }
 
             ##############################
             # Connections
 
-            members = r_job['members']
+            try:
+                members = r_job['members']
 
-            params = {'deep': '2'} # grados de conexion
-            url = "https://torre.bio/api/people/{}/network".format(username)
-            r = requests.get(url, params=params).json()
-            connections = r['graph']['nodes']
+                params = {'deep': '2'} # grados de conexion
+                url = "https://torre.bio/api/people/{}/network".format(username)
+                r = requests.get(url, params=params).json()
+                connections = r['graph']['nodes']
 
-            equal_connections = []
-            for member_a in members:
-                equal = False
-                for connection_a in connections:
-                    if member_a['person']['username'] == connection_a['metadata']['publicId']:
-                        equal = True
+                equal_connections = []
+                for member_a in members:
+                    equal = False
+                    for connection_a in connections:
+                        if member_a['person']['username'] == connection_a['metadata']['publicId']:
+                            equal = True
 
-                equal_connections.append(1) if equal else equal_connections.append(0)
+                    equal_connections.append(1) if equal else equal_connections.append(0)
 
-            for ind, member_a in enumerate(members):
-                member_a['accomplish'] = equal_connections[ind]
+                for ind, member_a in enumerate(members):
+                    member_a['accomplish'] = equal_connections[ind]
+
+            except:
+                members = { 'error': True }
 
             ##############################
 
